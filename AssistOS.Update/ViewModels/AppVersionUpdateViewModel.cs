d9ebd382ec5db5fd6e14488f8f2dc55c85929d78
Jonas Souza <jonassouza@live.com>
2023-12-27 09:01:03 -0300

    Adicionar arquivos de projeto.

---
+++ AssistOS.Update/ViewModels/AppVersionUpdateViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AssistOS.Update.ViewModels
+{
+    public class AppVersionUpdateViewModel
+    {
+        public Guid Id { get; set; }
+        public string Version { get; set; }
+        public string Description { get; set; }
+        public string LinkFile { get; set; }
+        public DateTime UpdateDate { get; set; } = DateTime.UtcNow;
+    }
+}
